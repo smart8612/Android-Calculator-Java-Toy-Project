efda5eced0859313b664acb113dba230884abb46
Han Jeong Taek <smart8612@gmail.com>
2018-01-15 16:56:56 +0900

    First Commit

---
+++ app/src/main/java/com/example/smart8612/calculator/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.smart8612.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.StringTokenizer;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView textView;
+
+    Button[] keyNum = new Button[10];
+    Button[] operator = new Button[7];
+
+    Integer[] btnNumID = {
+            R.id.btn0,
+            R.id.btn1, R.id.btn2, R.id.btn3,
+            R.id.btn4, R.id.btn5, R.id.btn6,
+            R.id.btn7, R.id.btn8, R.id.btn9
+    };
+    Integer[] btnOptID = {
+            R.id.btnP, R.id.btnM, R.id.btnMul,
+            R.id.btnDe, R.id.btnDot, R.id.btnC,
+            R.id.btnEval
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Load User Interface
+        textView = (TextView) findViewById(R.id.formularTextView);
+
+        for (int i = 0; i < btnNumID.length; i++) {
+            keyNum[i] = (Button) findViewById(btnNumID[i]);
+            keyNum[i].setOnClickListener(this);
+        }
+
+        for (int i = 0; i < btnOptID.length; i++) {
+            operator[i] = (Button) findViewById(btnOptID[i]);
+            operator[i].setOnClickListener(this);
+        }
+
+    }
+
+
+    // Call Back Function
+    @Override
+    public void onClick(View v) {
+        String tmpTxt = textView.getText().toString();
+
+        for (int i = 0; i < btnOptID.length; i++) {
+            if (v.getId() == btnOptID[i]) {
+                String keyTxt = operator[i].getText().toString();
+
+                switch (keyTxt) {
+                    case "C":
+                        tmpTxt = "";
+                        break;
+                    case "=":
+                        tmpTxt = eval(tmpTxt);
+                        break;
+
+                    default:
+                        if (tmpTxt.length() > 0) {
+                            Character tmplast = tmpTxt.charAt(tmpTxt.length() - 1);
+
+                            if (tmplast == '+' || tmplast == '-' || tmplast == '*' || tmplast == '/') {
+                                tmpTxt = tmpTxt.substring(0, tmpTxt.length() - 1);
+                                tmpTxt += keyTxt;
+                                break;
+
+                            } else if (tmplast == '.') {
+                                tmpTxt = tmpTxt.substring(0, tmpTxt.length() - 1);
+                                tmpTxt += keyTxt;
+                                break;
+
+                            } else {
+                                tmpTxt += keyTxt;
+                                break;
+                            }
+
+                        } else {
+                            break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < btnNumID.length; i++) {
+            if (v.getId() == btnNumID[i]) {
+                tmpTxt += keyNum[i].getText().toString();
+                break;
+            }
+
+        }
+
+        textView.setText(tmpTxt);
+        
+    }
+
+    public String eval(String exp) {
+        String Answer = "";
+
+        StringTokenizer st = new StringTokenizer(exp, "+-*/", true);
+
+        if (st.hasMoreTokens()) {
+            double firstNum = Double.parseDouble(st.nextToken().toString());
+            String operator = st.nextToken().toString();
+            double secondNum = Double.parseDouble(st.nextToken().toString());
+
+            switch (operator) {
+                case "+":
+                    Answer = String.valueOf(firstNum + secondNum);
+                    break;
+
+                case "-":
+                    Answer = String.valueOf(firstNum - secondNum);
+                    break;
+
+                case "*":
+                    Answer = String.valueOf(firstNum * secondNum);
+                    break;
+
+                case "/":
+                    Answer = String.valueOf(firstNum / secondNum);
+                    break;
+            }
+        }
+
+        return Answer;
+    }
+
+}
